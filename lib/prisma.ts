73a6332691b56a3d3c7e74ddd7a344872ba91dda
hoanggyen <147621826+galina723@users.noreply.github.com>
2026-08-04 16:54:35 +0700

    fix prisma 7 sqlite adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,19 @@
 import { PrismaClient } from "@/generated/prisma";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL!,
+});
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
